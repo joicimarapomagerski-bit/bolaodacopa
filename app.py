19fb649f45e4d5929c5c1d693b6b8e724f08cf9c
joicimarapomagerski-bit <joicimarapomagerski@gmail.com>
2026-06-15 03:19:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -878,13 +878,13 @@ with aba_regras:
     st.write("O sistema calcula os seus pontos comparando o seu palpite com o placar oficial do jogo. A pontuação não é cumulativa.")
 
     st.markdown("""
-    * **🎯 25 Pontos (Placar Exato):** Você acertou exatamente o número de gols de cada seleção.
+    * **25 Pontos (Placar Exato):** Você acertou exatamente o número de gols de cada seleção.
         * *Exemplo:* O jogo terminou 2x1. Você palpitou 2x1.
-    * **⚖️ 15 Pontos (Vencedor + Saldo de Gols):** Você acertou quem ganhou (ou se foi empate) **E** a diferença de gols, mas errou o placar exato.
+    * **15 Pontos (Vencedor + Saldo de Gols):** Você acertou quem ganhou (ou se foi empate) **E** a diferença de gols, mas errou o placar exato.
         * *Exemplo:* O jogo terminou 2x0 (saldo de 2). Você palpitou 3x1 (saldo de 2).
-    * **👍 10 Pontos (Acertou o Vencedor):** Você acertou apenas qual seleção venceu (ou se foi empate), mas errou o saldo e o placar.
+    * **10 Pontos (Acertou o Vencedor):** Você acertou apenas qual seleção venceu (ou se foi empate), mas errou o saldo e o placar.
         * *Exemplo:* O jogo terminou 1x0. Você palpitou 3x0 ou 2x1.
-    * **❌ 0 Pontos:** Você errou o resultado da partida (ex: apostou na vitória do Time A, mas deu empate ou Time B).
+    * **0 Pontos:** Você errou o resultado da partida (ex: apostou na vitória do Time A, mas deu empate ou Time B).
     """)
 
     # st.markdown("---")
